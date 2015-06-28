33656a18461682729d28299f82515584af16a3f5
mosiahvuxs <moisessrocha@gmail.com>
2015-06-28 10:56:34 -0300

    .

--- src/br/com/leitorarquivo/teste/NF40.java
+++ src/br/com/leitorarquivo/teste/NF40.java
@@ -1,5 +1,6 @@
 package br.com.leitorarquivo.teste;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -15,17 +16,18 @@ import java.time.format.FormatStyle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
 
-public class Ler3 {
+public class NF40 {
 
 	public static void main(String[] args) throws IOException, InterruptedException, ParseException {
 
-		System.out.println("Commons\n");
+		
 
-		/*
+		/*System.out.println("Commons\n");
 		 * System.out.println(new Date(System.currentTimeMillis()).getTime());
 		 * loadFileApacheCommons(new
 		 * File("D:\\leitor_arquivo\\embasa\\teste.txt"));
@@ -34,6 +36,22 @@ public class Ler3 {
 		 * System.out.println("----------------------------------------------\n")
 		 * ;
 		 */
+		
+		Path path = Paths.get("D:\\leitor_arquivo\\embasa\\teste.txt");
+		try (Stream<String> stream = Files.lines(Paths.get("D:\\leitor_arquivo\\embasa\\teste.txt"))) {
+		    stream
+		        .filter(line -> line.contains("826200000154    879400478209    499036590713    590000000000"))
+		        .map(String::trim)
+		        .forEach(System.out::println);
+		}
+		
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+		    long countPrints = reader
+		        .lines()
+		        .filter(line -> line.contains("826200000154    879400478209    499036590713    590000000000"))
+		        .count();
+		    System.out.println(countPrints);
+		}
 
 		System.out.println(LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM).withLocale(new Locale("pt", "br"))));
 
